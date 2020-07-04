991e0776072f33a93b1c11dc36168cacc2c42143
YasirNisar <yasirnisar7381@gmail.com>
2020-07-04 15:00:32 +0500

    code for show a list

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<ul>
+			<li>test</li>
+			<li>test2</li>
+			<li>test3</li>
+			<li>test4</li>
+			
+		</ul>
+
+		
+	</div>
+
+</body>
+</html>
